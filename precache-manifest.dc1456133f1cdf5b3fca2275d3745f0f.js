2551c9f13d882c037c4b4b2dea9dddea09f0f51e
Radoslaw Kolodziejczyk <radek940501@interia.pl>
2019-07-29 22:39:49 +0200

    Updates

---
+++ precache-manifest.dc1456133f1cdf5b3fca2275d3745f0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "1d7706232084d4b5fb91",
+    "url": "/weather-app/static/js/main.837a91ac.chunk.js"
+  },
+  {
+    "revision": "a5e203a85212c027c227",
+    "url": "/weather-app/static/js/2.987e4499.chunk.js"
+  },
+  {
+    "revision": "1d7706232084d4b5fb91",
+    "url": "/weather-app/static/css/main.540d0c71.chunk.css"
+  },
+  {
+    "revision": "ddcfd979285389afff95173380060f99",
+    "url": "/weather-app/index.html"
+  }
+];
